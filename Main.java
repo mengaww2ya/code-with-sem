be00444761adb9718957cb509c52536b912c4e27
mengaww2ya <147592488+mengaww2ya@users.noreply.github.com>
2023-10-14 10:55:36 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -9,9 +9,4 @@ public class Main {
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-    }
-}
+            // Press Shift+F9 to start debugging your code. We have set
